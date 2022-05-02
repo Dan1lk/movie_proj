18eefbdf86e18ce86c41302fa633f89b78b5feae
Dan1lk <prem_tank99@mail.ru>
2022-05-02 12:25:02 +0300

    Создание действия в админке

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,13 +1,15 @@
-from django.contrib import admin
+from django.contrib import admin, messages
 from .models import Movie
+from django.db.models import QuerySet
 # Register your models here.
 
 @admin.register(Movie)
 class MovieAdmin(admin.ModelAdmin):
-    list_display = ['name', 'rating', 'year', 'budget', 'rating_status']
-    list_editable = ['rating', 'year', 'budget']
+    list_display = ['name', 'rating', 'currency', 'budget', 'rating_status']
+    list_editable = ['rating', 'currency', 'budget']
     ordering = ['-rating', 'name']
     list_per_page = 5
+    actions = ['set_dollars', 'set_euro']
 
     @admin.display(ordering='rating', description='Статус')
     def rating_status(self, mov: Movie):
@@ -18,3 +20,16 @@ class MovieAdmin(admin.ModelAdmin):
         if mov.rating <= 85:
             return 'Зачет'
         return 'Топчик'
+
+    @admin.action(description='Установить валюту в доллар')
+    def set_dollars(self, request, qs: QuerySet):
+        qs.update(currency=Movie.USD)
+
+    @admin.action(description='Установить валюту в евро')
+    def set_euro(self, request, qs: QuerySet):
+        count_updated = qs.update(currency=Movie.EURO)
+        self.message_user(
+            request,
+            f'Было обновлено {count_updated} записей',
+            messages.ERROR
+        )

5d16271c112b1e47d90327d319f8343f3d39165f
Dan1lk <prem_tank99@mail.ru>
2022-05-02 11:23:50 +0300

    Настройки формы списка

--- movie_app/admin.py
+++ movie_app/admin.py
@@ -1,4 +1,10 @@
 from django.contrib import admin
 from .models import Movie
 # Register your models here.
-admin.site.register(Movie)
+
+@admin.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ['name', 'rating', 'year', 'budget']
+    list_editable = ['rating', 'year', 'budget']
+    ordering = ['-rating', 'name']
+    list_per_page = 3

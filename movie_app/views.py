631676a792175ff09928a889e5ab7174115cf8ca
Dan1lk <prem_tank99@mail.ru>
2022-05-02 10:16:11 +0300

    Функции агрегации F(Avg, Sum, Min, Max)

--- movie_app/views.py
+++ movie_app/views.py
@@ -1,12 +1,15 @@
 from django.shortcuts import render, get_object_or_404
 from . models import Movie
-from django.db.models import F
+from django.db.models import F, Sum, Max, Min, Count, Avg
 # Create your views here.
 
 def show_all_movie(request):
     movies = Movie.objects.order_by(F('year').desc(nulls_first=True), '-rating')
+    agg = movies.aggregate(Avg('budget'), Max('rating'), Min('rating'), Count('rating'))
+
     return render(request, 'movie_app/all_movies.html', {
-        'movies': movies
+        'movies': movies,
+        'agg': agg
     })
 
 def show_one_movie(request, slug_movie):

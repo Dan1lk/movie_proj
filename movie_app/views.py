25fc468dc0f7210e0a209e8b8a73f3c22053ea83
Dan1lk <prem_tank99@mail.ru>
2022-05-02 10:00:24 +0300

    init

---
+++ movie_app/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render, get_object_or_404
+from . models import Movie
+from django.db.models import F
+# Create your views here.
+
+def show_all_movie(request):
+    movies = Movie.objects.order_by(F('year').desc(nulls_first=True), '-rating')
+    return render(request, 'movie_app/all_movies.html', {
+        'movies': movies
+    })
+
+def show_one_movie(request, slug_movie):
+    movie = get_object_or_404(Movie, slug=slug_movie)
+    return render(request, 'movie_app/one_movie.html', {
+        'movie': movie
+    })
